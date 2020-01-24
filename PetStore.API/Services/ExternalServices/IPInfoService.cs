4a504716a9fa9c281dffbcdef20c8e51102706fd
jurosm <juros1234@gmail.com>
2020-01-25 00:44:38 +0100

    Add ipinfo service

--- PetStore.API/Services/ExternalServices/IPInfoService.cs
+++ PetStore.API/Services/ExternalServices/IPInfoService.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using IpInfo.Api.Client;
 using IpInfo.Api.Client.Models;
+using Newtonsoft.Json;
+using PetStore.API.Models.Response.ExternalServices;
 
 namespace PetStore.API.Services.ExternalServices
 {
     public class IPInfoService
     {
-        IIpInfoApiClient IPInfo;
+        HttpClient client;
         public IPInfoService()
         {
-            IPInfo = new IpInfoApiClient("https://localhost:5001");
+            client = new HttpClient();
         }
-        public BaseResponse<GetIpInfoResponse> GetLocation(string ipAddress)
+        public async Task<IPInfoResponse> GetLocation(string ipAddress)
         {
-            return IPInfo.GetIpInfo(ipAddress);
+            HttpResponseMessage httpResponse = await client.GetAsync("https://ipinfo.io/" + ipAddress + "?token=" + "237e92574bcfed");
+            string res = await httpResponse.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<IPInfoResponse>(res);
         }
     }
 }
--- PetStore.API/Services/ServicesConfiguration.cs
+++ PetStore.API/Services/ServicesConfiguration.cs
@@ -2,6 +2,7 @@
 using PetStore.API.Db;
 using PetStore.API.Services.CategorySystem;
 using PetStore.API.Services.CRUD;
+using PetStore.API.Services.OrderSystem;
 using PetStore.API.Services.ToySystem;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,10 @@ namespace PetStore.API.Services
             services.AddTransient<ContextWrapper<Toy>>();
             services.AddScoped<ToyRepository>();
             services.AddScoped<ToyService>();
+
+            services.AddTransient<ContextWrapper<Order>>();
+            services.AddScoped<OrderRepository>();
+            services.AddScoped<OrderService>();
         }
     }
 }
