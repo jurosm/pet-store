455680231ec0a2de24596dceb1798c290e0aaec5
jurosm <juros1234@gmail.com>
2020-01-26 16:07:32 +0100

    Add api url environment variable

--- pet-store/src/app/services/pet-store.service.ts
+++ pet-store/src/app/services/pet-store.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { Order } from '../models/order';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { LoginModel } from '../models/auth/loginModel';
 import { AuthResult } from '../models/auth/authResult';
 import { ErrorService } from './errorService';
+import { getLocaleExtraDayPeriods } from '@angular/common';
+import { GetToysParams } from '../models/toy/getToysParams';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +18,7 @@ export class PetStoreService {
 
   url: string;
   constructor(private httpClient: HttpClient, private errorService: ErrorService) {
-    this.url = 'https://localhost:5001/';
+    this.url = environment.apiUrl;
    }
 
   buy(order: Order) {
@@ -26,5 +29,19 @@ export class PetStoreService {
     return this.httpClient.post<AuthResult>(this.url + 'auth/login', model).pipe(
       catchError(this.errorService.handlerError)
     );
+  }
+
+  getToys(getToysParams: GetToysParams) {
+    const params = new HttpParams()
+    .set('page', getToysParams.page.toString())
+    .set('pageSize', getToysParams.itemsPerPage.toString());
+
+    if(getToysParams.order !== '') { params.set('order', getToysParams.order); }
+    if(getToysParams.category !== '') {  params.set('category', getToysParams.category); }
+    if(getToysParams.matchName !== '') { params.set('match', getToysParams.matchName); }
+
+    return this.httpClient.get(this.url + 'toys', {
+      params
+    });
   }
 }
