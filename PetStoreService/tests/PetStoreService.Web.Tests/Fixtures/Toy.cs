157525282fc6e73765fd9acbfcabdba33a3b6af0
Uros Malesevic <juros1234@gmail.com>
2024-09-21 23:28:18 +0200

    Fix failing test

--- PetStoreService/tests/PetStoreService.Web.Tests/Fixtures/Toy.cs
+++ PetStoreService/tests/PetStoreService.Web.Tests/Fixtures/Toy.cs
@@ -11,7 +11,7 @@ public class ToyFixture
             Name = new Bogus.Faker().Commerce.ProductName(),
             Description = new Bogus.Faker().Lorem.Sentence(),
             Price = decimal.Parse(new Bogus.Faker().Commerce.Price()),
-            ShortDescription = new Bogus.Faker().Lorem.Sentence(),
+            ShortDescription = new Bogus.Faker().Commerce.ProductName(),
             Quantity = new Bogus.Faker().Random.Int(),
         };
     }
