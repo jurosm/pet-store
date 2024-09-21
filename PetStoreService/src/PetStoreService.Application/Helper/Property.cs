5e4052ba12cb59ec95989c9d0e779383d75efed4
Uros Malesevic <juros1234@gmail.com>
2024-09-21 12:14:42 +0200

    Rename projects properly

--- PetStoreService/src/PetStoreService.Application/Helper/Property.cs
+++ PetStoreService/src/PetStoreService.Application/Helper/Property.cs
@@ -8,7 +8,7 @@ namespace PetStoreService.Application
     {
         public static DbSet<T> AccessOnCompile(PetStoreDBContext context)
         {
-            Type ContextType = Type.GetType("PetStoreService.Persistence.PetStoreDBContext, Persistence");
+            Type ContextType = Type.GetType("PetStoreService.Persistence.PetStoreDBContext, PetStoreService.Persistence");
             Type typeParameterType = typeof(T);
             PropertyInfo prop = ContextType.GetProperty(typeParameterType.Name);
             return prop.GetValue(context) as DbSet<T>;
